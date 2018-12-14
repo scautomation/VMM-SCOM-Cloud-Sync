3a988773f43d18bc7ee1ae1421a43e30028dbc57
scautomation <35973682+scautomation@users.noreply.github.com>
2018-12-14 16:02:23 -0600

    initial

---
+++ scripts/Host_CloudSync.ps1
@@ -0,0 +1,43 @@
+﻿#import VMM, OpsMgr and OpsMgrSDK Powershell Modules
+import-module virtualmachinemanager
+import-module operationsmanager
+Import-OpsMgrSDK
+
+#VMM, SCOM servers and Domain. Change to your environment
+$vmmserver = 'vmm01.sandlot.dom'
+$scomserver = 'om01.sandlot.dom'
+$domain = 'sandlot.dom'
+$mp = ""
+
+#get all VMM Hosts selecting their names and clusternames
+$hosts = get-vmhost -VMMServer $vmmserver | Select-Object computername,hostcluster,VMHostGroup
+
+#New-OMManagementPack -SDK $scomserver -DisplayName "Custom Cloud Groups" -Name "custom.cloud.groups"
+
+$mp = get-scommanagementpack -Name $mp
+
+
+foreach($vmhost in $hosts)
+{
+    $computer = $vmhost.computername
+    $cluster = $vmhost.hostcluster
+    $hostgroup = $vmhost.vmhostgroup
+
+    #VMM puts "all hosts" in front of each host group
+    $hostgroup = $hostgroup -replace "All Hosts\\",""
+
+ 
+    #I used this with 3 different host clusters, your needs may vary.       
+    $verify = get-scomgroup -displayname $hostgroup
+     
+        if($verify -eq $null)
+            {
+             $create = new-omcomputergroup -sdk $scomserver -MPname $mp.Name -computergroupname "HyperVHost.$hostgroup" -computergroupdisplayname "HyperVHost Group $hostgroup"
+             start-sleep -Seconds 400
+             $groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $verify.FullName -ComputerPrincipalName "$computer.$domain" -Verbose -Debug
+            } Else {$groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $verify.FullName -ComputerPrincipalName "$computer.$domain"}
+   
+    }
+
+
+    
---
+++ scripts/VM_cloudSync.ps1
@@ -0,0 +1,80 @@
+﻿<#
+Sync SCVMM Cloud VMs to SCOM groups
+Purpose of the script is to take Virtual Machines in VMM Clouds and put them in SCOM groups for maintenance mode and reporting. Also to add to DPM Protection groups.
+
+this script uses VMM powershell module and Tao Yang's OpsMgr custom powershell module for SCOM 2012 r2+
+
+by Billy York @scautomation
+www.systemcenterautomation.com
+#>
+
+# Set VMM and SCOM Servers
+$vmmserver = 'your VMM Server'
+$scomserver = 'your SCOM Server'
+$mpname = "management pack"
+$domain = "domain"
+
+#Set VMM Cloud groups and SCOM Groups
+$cloud1 = "Prod Cloud"
+$cloud2 = "Non Prod Cloud"
+$scomgroup1 = "Prod Cloud"
+$scomgroup2 = "Non Prod Cloud"
+
+#declare full group names for creation
+$scomgroup1Full = "Prod.Cloud"
+$scomgroup2Full = "Non.Prod.Cloud"
+
+#Import VMM, OpsMgr & OpsMgrSDK modules
+Import-OpsMgrSDK
+Import-Module virtualmachinemanager
+Import-Module operationsmanager
+
+#Get VMs from VMM Server, with Name, Cloud
+$vms = Get-SCVirtualMachine -VMMServer $vmmserver | Select-Object name, cloud
+
+foreach($vm in $vms)
+{
+    $computer = $vm.name
+    $cloud = $vm.Cloud
+    $cloudname = $cloud.name
+
+    
+    #If the VM is not assigned a cloud put in outfile
+    if($cloudname -eq $null)
+    {
+           $computer | out-file c:\temp\nocloudname.txt -Append
+     }
+
+  
+#sort Cloud1 VM into Cloud 1 SCOM Group
+    if($cloudname -eq $cloud1)
+    {
+       #get scom group
+       $verify = get-scomgroup -displayname $scomgroup1
+     
+       #if the scom group does not exist, create it, sleep, then add the VM to it.
+        if($verify -eq $null)
+            {
+             $create = new-omcomputergroup -sdk $scomserver -MPname $mpname -computergroupname $scomgroup1Full -computergroupdisplayname $scomgroup1
+             start-sleep -Seconds 360
+             $groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $create.fullname -ComputerPrincipalName $computer
+            } Else {$groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $verify.fullname -ComputerPrincipalName $computer}
+    }
+
+#sort Cloud2 VM into Cloud 2 SCOM Group
+    if($cloudname -eq $cloud2)
+    {
+        #get scom group 
+        $verify = get-scomgroup -displayname $scomgroup2
+     
+        #if the scom group does not exist, create it, sleep to allow DB propagation, then add the VM to it.
+        if($verify -eq $null)
+            {
+             $create = new-omcomputergroup -sdk $scomserver -MPname $mpname -computergroupname $scomgroup2full -computergroupdisplayname $scomgroup2
+             start-sleep -Seconds 360
+             $groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $create.fullname -ComputerPrincipalName $computer
+            } Else {$groupadd = New-OMComputerGroupExplicitMember -SDK $scomserver -GroupName $verify.fullname -ComputerPrincipalName $computer}
+            
+    }
+    
+}
